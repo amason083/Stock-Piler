b5b76510de185aa2a48e3adcdc7011f929741b71
AndrewCode <amason083@gmail.com>
2024-05-25 13:41:23 -0400

    jsx file

---
+++ Documents/react-dictionary/public/Components/WordCard/WordCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WordCard() {
+  return (
+    <div className="word-card">
+        <p>English: translation</p>
+        <p>German: Ubersetzung</p>
+    </div>
+  )
+}
--- Documents/react-dictionary/src/App.js
+++ Documents/react-dictionary/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import './Components/WordCard/WordCard.jsx';
 
 function App() {
   return (
